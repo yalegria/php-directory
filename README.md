c5cddb33523a9eda3cddd6d9a829f7fd2f32cf5f
yalegria <iguanadesignsnet@gmail.com>
2023-01-12 09:48:13 -0500

    add bootstrap, find index file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # php directory
+
+This simple PHP script is intented to be run on a web server. It will traverse a specific directory and read all subdirectories. Then it will find the index file in each directory.
+
+At the end, the script will display the list of directories with a link to open out the directory application.
--- index.php
+++ index.php
@@ -1,20 +1,42 @@
 <?php
-echo "Traversing...\n";
-$dir = "ssw";
+$dir = "SSW/*";
+?>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <table class="table">
+    <?php
+    $all = glob($dir);
 
 
-$all = glob($dir);
+    if ($all) {
+        foreach ($all as $ff) {
+            if (is_dir($ff)) {
+                $indexfile = glob("{$ff}/index*");
+                ?>
+                <tr>
+                    <td><?php echo "{$ff} \n"; ?></td>
+                    <td>
+                        <?php if ($indexfile) { ?>
+                        <a href="<?php echo $indexfile[0]; ?>" target="_blank">View</a>
+                        <?php } else { ?>
+                            No Index
+                        <?php } ?>
+                    </td>
+                </tr>
 
-if ($all) {
-    foreach ($all as $ff) {
-        if (is_dir($ff)) {
-            echo "{$ff} \n";
+            <?php 
+            }
         }
     }
-}
-else {
-    echo 'No directory to traverse';
-}
-
-
-?>
+    else {
+        echo 'No directory to traverse';
+    }
+    ?>
+    </table>
+</div>
+</body>
+</html>
